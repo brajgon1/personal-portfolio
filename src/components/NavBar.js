bf9d5f0927e88e5e34927cffa4d2ebc327710c30
Brandon Gonzalez <brajgon@gmail.com>
2024-09-25 20:45:22 -0600

    modified navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { NavBar, Container, Nav } from "react-bootstrap";
-import { NavBar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 export const NavBar = () => {
-    return (
-        <NavBar expand='lg'>
-            <Container>
-                <Navbar.Brand href='#home'><img src={''} alt='logo'/></Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link href='#home'>Home</Nav.Link>
-                        <Nav.Link href='#about'>Link</Nav.Link>
-                        <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-                            <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-                            <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
-                            <NavDropdown.Item href='#action/3.3'>Something else</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </NavBar>
-    )
-}
+  return (
+    <NavBar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={""} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+                <a href="#"><img src={} alt=""/></a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </NavBar>
+  );
+};
